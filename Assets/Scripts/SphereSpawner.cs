98b4c799ceb4f4e8a2909a5bb2b2b11d2213f2c5
bmw730td <bmw037td@gmail.com>
2023-10-27 21:19:41 +0300

    Task 1-6 fixed

--- Assets/Scripts/SphereSpawner.cs
+++ Assets/Scripts/SphereSpawner.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class SphereSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject _sphereTemplate;
+    [SerializeField] private Transform _sphereTemplate;
     [SerializeField] private float _spawnTime;
     [SerializeField] private Quaternion _rotation;
 
     private Transform[] _spawnpoints;
+    private WaitForSeconds _waitTime;
 
     private void Start()
     {
+        _waitTime = new WaitForSeconds(_spawnTime);
         _spawnpoints = new Transform[transform.childCount];
 
         for (int i = 0; i < transform.childCount; i++)
@@ -28,7 +30,7 @@ public class SphereSpawner : MonoBehaviour
         {
             Instantiate(_sphereTemplate, _spawnpoints[Random.Range(0, _spawnpoints.Length)].position, _rotation);
             
-            yield return new WaitForSeconds(_spawnTime);
+            yield return _waitTime;
         }
     }
 }
